f4585eff0903e81f5f49cdbc97deab78fc50f913
gamemaster2b <gamemaster2b@gmail.com>
2022-11-16 22:21:34 +0000

    ENG102 WEEK 6 COMPLETE

---
+++ ENG102/WEEK 6/QUESTION TWO.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    float val, con;
+    char unit;
+    printf("Please enter degrees (example 18C or 50F):");
+    scanf("%f%c",&val,&unit);
+
+    if( unit == 'c' || unit == 'C'){
+        con = val * 1.8 +32;
+        printf("%f degrees Celcius = %f degrees Fahrenheit", val, con);
+    }
+    if( unit == 'F' || unit == 'f'){
+        con = (val-32) / 1.8;
+        printf("%f degrees Fahrenheit = %f degrees Celcius", val, con);
+    }
+}
